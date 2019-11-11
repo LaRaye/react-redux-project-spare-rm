0119c74bb05278e4b95efcbe3396bbdc84c00261
LaRaye <lnj2105@gmail.com>
2019-11-10 19:28:47 -0500

    initial home and stayslist components and routes

---
+++ src/containers/StaysList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StaysList extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Stays</h1>
+      </div>
+    )
+  }
+}
+
+export default StaysList;
