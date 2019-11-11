f2732e1bf0c6b728c7affa070f78af2b0961d68f
LaRaye <lnj2105@gmail.com>
2019-11-10 19:30:47 -0500

    staysreducer

---
+++ src/reducers/staysReducer.js
@@ -0,0 +1,18 @@
+const stayReducer = (state = { stays: [], loading: false }, action) => {
+  switch (action.type) {
+    case 'LOADING_STAYS':
+      return {
+        ...state,
+        stays: [...state.stays],
+        loading: true
+      }
+    case 'ADD_STAYS':
+      return {
+        ...state,
+        stays: [...state.stays],
+        loading: false
+      }
+    default:
+      return state;
+  }
+}
