f595dc4b817f2f351032a27098dc780edcb1d887
LaRaye <lnj2105@gmail.com>
2019-11-11 23:18:41 -0500

    fetch stays from rails api

--- src/containers/StaysList.js
+++ src/containers/StaysList.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { fetchStays } from '../actions/stays';
+import { connect } from 'react-redux';
 
 class StaysList extends Component {
+
+  componentDidMount() {
+    this.props.fetchStays()
+  }
+
   render() {
+    console.log(this.props.stays)
+    if (this.props.stays.length === 0) {
+      return <p>Loading...</p>
+    }
+
     return(
       <div>
         <h1>Stays</h1>
+        {this.props.stays.map(stay => <p>{stay.title}</p>)}
       </div>
     )
   }
 }
 
-export default StaysList;
+const mapStateToProps = state => {
+  return {
+    stays: state.stays
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchStays }
+  )(StaysList);
