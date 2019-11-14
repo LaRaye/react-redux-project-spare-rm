b5b10abe03309a940b985e44ce0b1fbc17de71b5
LaRaye <lnj2105@gmail.com>
2019-11-14 01:16:35 -0500

    continue building stay show

--- src/components/Stay.js
+++ src/components/Stay.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux';
 
-const Stay = () => {
+const Stay = (props) => {
+  if (!props.stay) {
+    return <p>Loading...</p>
+  }
+
+// what if stay does not exist?
+
   return (
     <div>
-      <h1>This Stay</h1>
+      <h1>{props.stay.title}</h1>
     </div>
   )
 }
 
 const mapStateToProps = (state, props) => {
-  const id = props.match.params.id;
+  const id = parseInt(props.match.params.id);
   const stay = state.stays.filter(stay => stay.id === id)[0];
   return {
-    stay
+    stay: stay
   };
 };
 
