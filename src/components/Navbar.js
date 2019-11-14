98dc622ca0dce62f6d4ac313a69dae4379e5a9e4
LaRaye <lnj2105@gmail.com>
2019-11-14 16:23:33 -0500

    add navbar and initial stylization

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link  } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav `
+  li {
+    list-style: none
+  }
+
+  ul {
+    display: flex;
+    justify-content: space-evenly;
+  }
+`
 
 const Navbar = () => (
-  <nav>
-    <ul>
-      <Link to="/">Home</Link>
-      <Link to="/stays">Stays</Link>
-      <Link to="/stays/new">List a Stay</Link>
-    </ul>
-  </nav>
+  <StyledNav>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/stays">Stays</Link></li>
+        <li><Link to="/stays/new">List a Stay</Link></li>
+      </ul>
+    </nav>
+  </StyledNav>
 )
 
 export default Navbar;
