196b5083f024e9bd0f80e0fc05c1bbddf23279f1
LaRaye <lnj2105@gmail.com>
2019-11-17 18:51:50 -0500

    stub usersreducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore, applyMiddleware, compose } from 'redux';
 import staysReducer from './reducers/staysReducer';
+import usersReducer from './reducers/usersReducer';
 import App from './App';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,5 +1,8 @@
 const usersReducer = (state = { users: [], loading: false }, action) => {
-
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
 
 export default usersReducer;
